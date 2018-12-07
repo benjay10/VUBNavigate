08baeb48b2254022a211d7e4b1218b4c20579873
benjay10 <benbehaeghe@gmail.com>
2018-12-07 15:21:31 +0100

    Cleanup

--- js/startup.js
+++ js/startup.js
@@ -33,9 +33,7 @@ window.addEventListener("load", (event) => {
 		// Initialisation of the database service first
 
 		vubn.services.database = new DatabaseService();
-		vubn.services.database.init().then((db) => resolve(vubn.services.database));
-
-		//resolve(vubn.services.database);
+		vubn.services.database.init().then((db) => resolve(vubn.services.database)).catch(reject);
 
 	})).then((dbService) => {
 
